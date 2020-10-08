60a67ec7c5430f6cb775b6c30c1013c13571e1ab
alezhka <alezhk@gmail.com>
2020-10-08 13:37:10 +0300

    Bump flutter min to 1.12.13.

--- android/src/main/kotlin/com/github/alezhka/flutter_survicate/FlutterSurvicatePlugin.kt
+++ android/src/main/kotlin/com/github/alezhka/flutter_survicate/FlutterSurvicatePlugin.kt
@@ -38,25 +38,9 @@ class FlutterSurvicatePlugin: FlutterPlugin, MethodCallHandler {
       eventChannel.setStreamHandler(messageStreamHandler)
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    companion object {
-      @JvmStatic
-      fun registerWith(registrar: Registrar) {
-        val instance = FlutterSurvicatePlugin()
-        instance.context = registrar.context()
-        instance.methodChannel = MethodChannel(registrar.messenger(), "survicate")
-        instance.methodChannel.setMethodCallHandler(instance)
-        instance.eventChannel = EventChannel(registrar.messenger(), "events")
-        instance.eventChannel.setStreamHandler(instance.messageStreamHandler)
-      }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+      methodChannel.setMethodCallHandler(null)
+      context = null
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
